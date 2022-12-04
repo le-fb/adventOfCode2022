23522ea584941083c53f8edf69bd0de8ac859cb8
Kay Menkens-Siemers <kay_men@uni-bremen.de>
2022-12-04 23:05:00 +0100

    aufgabe 2

--- kayms/2/main.c
+++ kayms/2/main.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <assert.h>
 
-#define INPUT_FILE_NAME "../../2/input"
+#define INPUT_FILE_NAME "../../kayms/2/input"
 #define MAX_COUNT_ELF 1000
 #define LINE_BUFFER_SIZE 100
 
+#define ROCK 1
+#define PAPER 2
+#define SCISSOR 3
+
 int main()
 {
+
+    int elfMove = -1;
+    int myMove = -1;
+    int outcome = -1;
+    int myPoints = 0;
 
     FILE *file = fopen(INPUT_FILE_NAME, "r");
     if (file == NULL)
@@ -21,11 +31,70 @@ int main()
     {
         if (ch != '\n')
         {
+            if (ch != ' ')
+            {
+                // read move
+                if (elfMove < 0)
+                {
+                    // reading elf move
+                    elfMove = (int)ch - 64; // ascii A -> 65
+                }
+                else
+                {
+                    // reading my move
+                    outcome = (int)ch - 88; // 0 -> lose , 1 -> draw, 2 -> win
+                    if (outcome == 2)
+                    {
+                        // win
+                        myMove = elfMove + 1;
+                        if (myMove == 4)
+                        {
+                            myMove = 1;
+                        }
+                    }
+                    else if (outcome == 1)
+                    {
+                        // draw
+                        myMove = elfMove;
+                    }
+                    else
+                    {
+                        // lose
+                        myMove = elfMove - 1;
+                        if (myMove == 0)
+                        {
+                            myMove = 3;
+                        }
+                    }
+                }
+            }
         }
         else
         {
+            assert(myMove > 0 && myMove < 4);
+            assert(elfMove > 0 && elfMove < 4);
+
+            // analysis move
+            if (myMove == elfMove)
+            {
+                myPoints += myMove + 3;
+            }
+            else if ((myMove == PAPER && elfMove == ROCK) || (myMove == SCISSOR && elfMove == PAPER) || (myMove == ROCK && elfMove == SCISSOR))
+            {
+                // i win
+                myPoints += myMove + 6;
+            }
+            else
+            {
+                // i lose
+                myPoints += myMove;
+            }
+            elfMove = -1;
+            myMove = -1;
         }
         ch = getc(file);
     }
     fclose(file);
+
+    printf("points %i\n", myPoints);
 }
