d5c16a2d36717a5d5b337355bf0363265356eda7
Leon Faßbinder <le.fb98@gmail.com>
2022-12-06 00:51:57 +0100

    fixing comment in c-utils for file reading

--- kayms/utils/utils.c
+++ kayms/utils/utils.c
@@ -40,7 +40,7 @@ int readFile(char *filename, char ***content, int *lines)
 
     *lines = tempLines;
 
-    // makes lines
+    // makes points
     int lastIndex = 0;
     int lineIndex = 0;
     (*content) = malloc(tempLines * sizeof(void *));
