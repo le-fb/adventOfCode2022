ae29b0836c690be34713045a2f5799c2623ede23
Tim Stoetzel <timstoetzel@web.de>
2022-12-11 11:19:08 +0100

    add 6

---
+++ the_cooler_tim/6/6_2.jl
@@ -0,0 +1,25 @@
+f = readlines("input")
+
+buffer = []
+str = ""
+index = 0
+packetLength = 14 
+
+for line in f 
+    global str *= line
+end
+
+for i in 1:length(str)
+    # create a set from the buffer since each element is unique per definition
+    if(length(Set(buffer)) == packetLength && index == 0) # check if the sets cardinality equals the packet lenght (4)
+        global index = i-1
+    end
+    if(length(buffer) < packetLength) # fill the buffer
+        push!(buffer, str[i])
+    elseif(length(buffer) == packetLength && index == 0) # move the buffer by one 
+        popfirst!(buffer)
+        push!(buffer, str[i])
+    end
+end
+
+print("First marker after: ", index)
